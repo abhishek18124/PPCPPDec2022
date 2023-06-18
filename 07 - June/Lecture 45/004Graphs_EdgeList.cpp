56a5b55fb70e952fdbb0d0325de0839f7191716b
abhishek18124 <abhishek18124@iiitd.ac.in>
2023-06-18 17:41:21 +0530

    Lecture 45

---
+++ 07 - June/Lecture 45/004Graphs_EdgeList.cpp
@@ -0,0 +1,73 @@
+/*
+
+Implementation of the weighted graph using edge list.
+
+Note : each vertex in the graph is generic.
+
+*/
+
+#include<iostream>
+#include<vector>
+#include<set>
+
+using namespace std;
+
+template <typename T>
+class edge {
+public :
+
+	T u;
+	T v;
+	int w;
+
+	edge(T u, T v, int w) {
+		this->u = u;
+		this->v = v;
+		this->w = w;
+	}
+};
+
+template <typename T>
+class graph {
+
+	set<T>  vertexSet;        // to store the graph representation
+	vector<edge<T>> edgeList; // to store the graph representation
+
+public :
+
+	void addEdge(T u, T v, int w) {
+		// add an edge of weight 'w' b/w 'u' and 'v'
+		edgeList.push_back(edge<T>(u, v, w));
+
+
+		// edgeList.push_back(edge<T>(v, u, w)); // do this if you want undirected graph
+
+		vertexSet.insert(u);
+		vertexSet.insert(v);
+	}
+
+	void print() {
+		for (edge<T> edge : edgeList) {
+			cout << edge.u << "->" << edge.v << " : " << edge.w << endl;
+		}
+	}
+};
+
+
+int main() {
+
+	graph<char> g;
+
+	// assume we are building directed weighted graph
+
+	g.addEdge('A', 'B', 7);
+	g.addEdge('A', 'C', 1);
+	g.addEdge('B', 'D', 2);
+	g.addEdge('C', 'D', 9);
+	g.addEdge('C', 'E', 6);
+	g.addEdge('D', 'E', 5);
+
+	g.print();
+
+	return 0;
+}
